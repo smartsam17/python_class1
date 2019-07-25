581d8eb6aa52f2766f07d6290b5f37498c765039
smartsam17 <53172630+smartsam17@users.noreply.github.com>
2019-07-25 11:51:41 +0530

    Update class1_programs.py

--- class1_programs.py
+++ class1_programs.py
@@ -1,1 +1,7 @@
+#print as comma sequece in a single line,divisble by 7 but not a multiple of 5 in between 2000 and 3200,
 
+numbers = [];
+for i in range(2000, 3201):
+    if i%7 == 0 and i%5 != 0 :
+        numbers.append(str(i))
+print (",".join(numbers))
